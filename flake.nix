fbbadf844c8380212308ae94a5c58b5660958636
Martin Schenck <martinschenck@fastmail.com>
2025-11-25 11:08:09 +0100

    add flake to manage hugo dependency

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Static blog at blog.schemar.net";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShellNoCC {
+          packages = [
+            pkgs.hugo
+          ];
+        };
+      }
+    );
+}
